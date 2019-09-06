e275a52e53f333d1d45d696bfd308599dee85355
Руслан <agishevrus@gmail.com>
2019-09-06 18:40:44 +0300

    Продолжение изучения темы по 7 модулю и выполнения ДЗ

--- Homework_07/Program.cs
+++ Homework_07/Program.cs
@@ -30,21 +30,82 @@ namespace Homework_07
         {
             var recordsRepository = new RecordsRepository("data.csv");
 
-            recordsRepository.PrintAllRecords();
-
-            Console.ReadKey();
-            Console.WriteLine();
-
-            recordsRepository.AddRecord(new Record(recordsRepository.Count() + 1, "Пример 2", "Тест", DateTime.Now, DateTime.Now));
-            recordsRepository.AddRecord(new Record(recordsRepository.Count() + 1, "Пример 3", "Тест 3 3", DateTime.Now, DateTime.Now));
-
-            recordsRepository.PrintAllRecords();
-            Console.ReadKey();
-
-            recordsRepository.SaveRecords();
-            Console.ReadKey();
 
 
+            bool flag = true;
+
+
+            while (flag)
+            {
+                Console.Write("Меню:" +
+                "\n1. Добавить запись." +
+                "\n2. Найти запись." +
+                "\n3. Редактироать запись." +
+                "\n4. Удалить запись." +
+                "\n5. Вывести все записи в консоль." +
+                "\n6. Сохранить записи и выйти." +
+                "\nВаш выбор: ");
+
+                int mode;
+
+                while (!int.TryParse(Console.ReadLine(), out mode) || mode < 1 || mode > 6)
+                {
+                    Console.Write("Введено недопустимое значение." +
+                            "\nПовторите ввод: ");
+                }
+
+                int number;
+
+                switch (mode)
+                {
+                    case 1:
+                        Console.Write("Введите заголовок записи: ");
+                        var caption = Console.ReadLine();
+
+                        Console.Write("Введите текст записи: ");
+                        var description = Console.ReadLine();
+
+                        recordsRepository.AddNewRecord(caption, description);
+                        break;
+                    case 2:
+                        Console.Write("Введите номер записи: ");
+                        number = int.Parse(Console.ReadLine());
+
+
+
+
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        Console.Write("Введите номер записи: ");
+                        number = int.Parse(Console.ReadLine());
+
+                        var r =  recordsRepository.FindRecordByNumber(number);
+
+                        recordsRepository.DeleteRecord(r);
+
+                        break;
+                    case 5:
+                        recordsRepository.PrintRecords();
+
+                        break;
+                    case 6:
+                        recordsRepository.SaveRecords("data.csv");
+                        Console.WriteLine("Данные сохранены. Нажмите любую клавишу для выхода.");
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Что-то пошло не так.");
+                        break;
+                }
+
+
+
+
+
+
+            }
         }
     }
 }
--- Homework_07/RecordsRepository.cs
+++ Homework_07/RecordsRepository.cs
@@ -1,32 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
 namespace Homework_07
 {
     struct RecordsRepository
     {
-        private List<Record> records; // Список записей
-        private string path; // Путь к файлу с записями
-        private string[] titles; // Массив, храняий заголовки полей. используется в PrintDbToConsole
+        private List<Record> records;
+        private string[] titles;
 
         public RecordsRepository(string path)
         {
-            this.path = path;
+            //this.path = path;
             this.titles = new string[0];
             this.records = new List<Record>();
-            this.LoadRecords();
+            this.LoadRecords(path);
         }
 
         /// <summary>
         /// Загрузка записей из файла
         /// </summary>
-        private void LoadRecords()
+        private void LoadRecords(string path)
         {
-            using (var sr = new StreamReader(this.path))
+            using (var sr = new StreamReader(path))
             {
                 titles = sr.ReadLine().Split(',');
 
@@ -34,45 +33,42 @@ namespace Homework_07
                 {
                     string[] args = sr.ReadLine().Split(',');
 
-                    AddRecord(new Record(Convert.ToInt32(args[0]), args[1], args[2], Convert.ToDateTime(args[3]), Convert.ToDateTime(args[4])));
+                    records.Add(new Record(Convert.ToInt32(args[0]), args[1], args[2], Convert.ToDateTime(args[3]), Convert.ToDateTime(args[4])));
                 }
             }
         }
 
-        /// <summary>
-        /// Метод добавления записи в хранилище
-        /// </summary>
-        /// <param name="ConcreteWorker">Запись</param>
-        public void AddRecord(Record concreteRecord)
+        public void AddNewRecord(string caption, string descripition)
         {
-            records.Add(concreteRecord);
+            int id;
+            if (records.Count != 0)
+            {
+                int[] number = records.Select(x => x.Number).ToArray();
+                int[] missingNumbers = Enumerable.Range(number[0], number[number.Length - 1]).Except(number).ToArray();
+                id = missingNumbers.Length == 0 ? number.Max() + 1 : missingNumbers.FirstOrDefault();
+            }
+            else
+            {
+                id = 1;
+            }
+            var record = new Record(id, caption, descripition, DateTime.Now, DateTime.Now);
+
+            records.Add(record);
+            Console.WriteLine("Добавлена запись:");
+            Console.WriteLine($"{record.Number,6} {record.Caption,15} {record.Description,25} {record.CreateDateTime,20} {record.LastModifyDateTime,20}");
         }
 
-
-        public void CreateRecord()
+        public void PrintRecords()
         {
+            Console.WriteLine($"{this.titles[0],6} {this.titles[1],15} {this.titles[2],25} {this.titles[3],20} {this.titles[4],20}");
 
+            foreach (var item in records)
+            {
+                Console.WriteLine($"{item.Number,6} {item.Caption,15} {item.Description,25} {item.CreateDateTime,20} {item.LastModifyDateTime,20}");
+            }
         }
 
-        public void ReadRecord()
-        {
-
-        }
-
-        public void UpdateRecord()
-        {
-
-        }
-
-        public void DeleteRecord()
-        {
-
-        }
-
-        /// <summary>
-        /// Сохранение текущих записей хранилища в файл
-        /// </summary>
-        public void SaveRecords()
+        public void SaveRecords(string path)
         {
             using (var sw = new StreamWriter(path, false))
             {
@@ -98,26 +94,50 @@ namespace Homework_07
             }
         }
 
-        /// <summary>
-        /// Вывод записей из хранилища в консоль
-        /// </summary>
-        public void PrintAllRecords()
+        public void UpdateRecord(Record record)
         {
-            Console.WriteLine($"{this.titles[0],6} {this.titles[1],15} {this.titles[2],25} {this.titles[3],20} {this.titles[4],20}");
 
-            foreach (var item in records)
-            {
-                Console.WriteLine($"{item.Number,6} {item.Caption,15} {item.Description,25} {item.CreateDateTime,20} {item.LastModifyDateTime,20}");
-            }
         }
 
+
         /// <summary>
-        /// Количество записей в хранилище
+        /// Удаление записи по номеру
         /// </summary>
-        /// <returns>Возвращает количество записей в хранилище</returns>
+        /// <param name="number">Запись</param>
+        public void DeleteRecord(Record record)
+        {
+            records.Remove(record);
+        }
+
+        public void SortBy()
+        {
+
+        }
+
+        public Record FindRecordByCaption(string caption)
+        {
+            return new Record();
+        }
+
+        public Record FindRecordByDescription(string description)
+        {
+            return new Record();
+        }
+
+        public Record FindRecordByNumber(int number)
+        {
+            return records.Where(x => x.Number == number).FirstOrDefault();
+        }
+
+        public Record FindRecordByDate(DateTime dateFrom, DateTime dateTo)
+        {
+            return new Record();
+        }
+
         public int Count()
         {
             return records.Count;
         }
+
     }
 }
