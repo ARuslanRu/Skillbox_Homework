3c1e6548b403e5cfbd9f77427eeeda2b40a6b544
Руслан Агишев <agishevrus@gmail.com>
2019-10-19 21:10:02 +0300

    ДЗ 9. Частично переделал.

---
+++ Homework_09/MyBot.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.ReplyMarkups;
+using Homework_09.Model;
+
+namespace Homework_09
+{
+    class MyBot
+    {
+        #region Fields
+
+        /// <summary>
+        /// Бот
+        /// </summary>
+        TelegramBotClient bot;
+        /// <summary>
+        /// Токен
+        /// </summary>
+        string token;
+        /// <summary>
+        /// Репозиторий кнопок
+        /// </summary>
+        List<BotButton> buttons;
+
+        #endregion
+
+        #region Constructors
+        public MyBot()
+        {
+            #region Прокси
+            //Подходящий прокси найти не удалось
+            //https://hidemyna.me/ru/proxy-list/?maxtime=250#list
+
+            //var proxy = new WebProxy()
+            //{
+            //    Address = new Uri($"http://109.86.229.189:8080"),
+            //    UseDefaultCredentials = false
+            //};
+
+            //var httpClienHandler = new HttpClientHandler()
+            //{
+            //    Proxy = proxy
+            //};
+
+            //HttpClient hc = new HttpClient(httpClienHandler);
+            #endregion
+
+            token = File.ReadAllText("token");
+            bot = new TelegramBotClient(token);
+            buttons = Repository.getInstance().Buttons;
+            //при использовании прокси
+            //bot = new TelegramBotClient(token, hc);
+        }
+        #endregion
+
+        #region Private Methods      
+
+        /// <summary>
+        /// Сохранение документа полученного ботом в сообщение
+        /// </summary>
+        /// <param name="bot"></param>
+        /// <param name="fileId">Идентификатор файла</param>
+        /// <param name="path"></param>
+        private async void DownloadAsync(string fileId, string path)
+        {
+            var file = await bot.GetFileAsync(fileId);
+            using (FileStream fs = new FileStream($"_{path}", FileMode.Create))
+            {
+                await bot.DownloadFileAsync(file.FilePath, fs);
+            }
+        }
+
+        private void UpdateListener(object sender, UpdateEventArgs e)
+        {
+            switch (e.Update.Type)
+            {
+                case UpdateType.Unknown:
+                    break;
+                case UpdateType.Message:
+                    ResponseOnMessage(e);
+                    break;
+                case UpdateType.InlineQuery:
+                    break;
+                case UpdateType.ChosenInlineResult:
+                    break;
+                case UpdateType.CallbackQuery:
+                    ResponseOnCallbackQerry(e);
+                    break;
+                case UpdateType.EditedMessage:
+                    break;
+                case UpdateType.ChannelPost:
+                    break;
+                case UpdateType.EditedChannelPost:
+                    break;
+                case UpdateType.ShippingQuery:
+                    break;
+                case UpdateType.PreCheckoutQuery:
+                    break;
+                case UpdateType.Poll:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Ответ на нажатие по кнопке инлайн клавиатуры
+        /// </summary>
+        /// <param name="e"></param>
+        private void ResponseOnCallbackQerry(UpdateEventArgs e)
+        {
+            string text = $"{DateTime.Now.ToLongTimeString()} | Type: {e.Update.Type.ToString()} | Data: {e.Update.CallbackQuery.Data}";
+            Console.WriteLine(text);
+
+            var callbackQuery = e.Update.CallbackQuery;
+            //var buttons = Repository.getInstance().Buttons;
+
+            if (callbackQuery.Data == "0")
+            {
+                bot.EditMessageTextAsync(
+                    callbackQuery.Message.Chat.Id,
+                    callbackQuery.Message.MessageId,
+                    "Основное меню",
+                    replyMarkup: StartKeyboard());
+            }
+            else
+            {
+                Int32.TryParse(callbackQuery.Data, out int id);
+
+                var row = buttons.Where(x => x.ParentId == id).OrderBy(x => x.Row).ThenBy(x => x.Column).GroupBy(x => x.Row).ToList();
+
+                var inlineKeyboard = new List<List<InlineKeyboardButton>>();
+                foreach (var item in row)
+                {
+                    var listRowButtons = new List<InlineKeyboardButton>();
+                    foreach (var i in item)
+                    {
+                        listRowButtons.Add(InlineKeyboardButton.WithCallbackData($"{i.ButtonName}", i.Id.ToString()));
+                    }
+                    inlineKeyboard.Add(listRowButtons);
+                }
+
+                //Возврат в основное меню
+                var parentId = buttons.Where(x => x.Id == id).First().ParentId;
+                BotButton backButton;
+
+                if (parentId != 0)
+                {
+                    backButton = buttons.Where(x => x.Id == parentId).First();
+                }
+                else
+                {
+                    backButton = new BotButton { Id = 0, ParentId = 0, ButtonName = "Оснвоное меню" };
+                }
+
+                inlineKeyboard.Add(new List<InlineKeyboardButton>()
+                {
+                    InlineKeyboardButton.WithCallbackData($"<< Назад в {backButton.ButtonName}", backButton.Id.ToString())
+                });
+
+                bot.EditMessageTextAsync(
+                    callbackQuery.Message.Chat.Id,
+                    callbackQuery.Message.MessageId,
+                    buttons.Where(x => x.Id == id).First().ButtonName,
+                    replyMarkup: new InlineKeyboardMarkup(inlineKeyboard));
+            }
+        }
+
+        /// <summary>
+        /// Ответ на сообщение
+        /// </summary>
+        /// <param name="bot"></param>
+        /// <param name="e"></param>
+        private void ResponseOnMessage(UpdateEventArgs e)
+        {
+            string text = $"{DateTime.Now.ToLongTimeString()} | Type: {e.Update.Type.ToString()} | Text: {e.Update.Message.Text}";
+            Console.WriteLine(text);
+
+            switch (e.Update.Message.Type)
+            {
+                case MessageType.Unknown:
+                    break;
+                case MessageType.Text:
+                    switch (e.Update.Message.Text.Split(' ').First())
+                    {
+                        case "/inline":
+                            bot.SendTextMessageAsync(e.Update.Message.Chat.Id, "Основное меню", replyMarkup: StartKeyboard());
+                            break;
+                        default:
+                            const string usage = "Помощь:" +
+                                "\n/inline - получить инлайн клавиатуру";
+                            bot.SendTextMessageAsync(e.Update.Message.Chat.Id, usage, replyMarkup: new ReplyKeyboardRemove());
+                            break;
+                    }
+                    break;
+                case MessageType.Photo:
+                    bot.SendTextMessageAsync(e.Update.Message.Chat.Id, text);
+                    break;
+                case MessageType.Audio:
+                    bot.SendTextMessageAsync(e.Update.Message.Chat.Id, text);
+                    break;
+                case MessageType.Video:
+                    bot.SendTextMessageAsync(e.Update.Message.Chat.Id, text);
+                    break;
+                case MessageType.Voice:
+                    bot.SendTextMessageAsync(e.Update.Message.Chat.Id, text);
+                    break;
+                case MessageType.Document:
+                    bot.SendTextMessageAsync(e.Update.Message.Chat.Id, text);
+                    break;
+                case MessageType.Sticker:
+                    bot.SendTextMessageAsync(e.Update.Message.Chat.Id, text);
+                    break;
+                case MessageType.Location:
+                    break;
+                case MessageType.Contact:
+                    break;
+                case MessageType.Venue:
+                    break;
+                case MessageType.Game:
+                    break;
+                case MessageType.VideoNote:
+                    break;
+                case MessageType.Invoice:
+                    break;
+                case MessageType.SuccessfulPayment:
+                    break;
+                case MessageType.WebsiteConnected:
+                    break;
+                case MessageType.ChatMembersAdded:
+                    break;
+                case MessageType.ChatMemberLeft:
+                    break;
+                case MessageType.ChatTitleChanged:
+                    break;
+                case MessageType.ChatPhotoChanged:
+                    break;
+                case MessageType.MessagePinned:
+                    break;
+                case MessageType.ChatPhotoDeleted:
+                    break;
+                case MessageType.GroupCreated:
+                    break;
+                case MessageType.SupergroupCreated:
+                    break;
+                case MessageType.ChannelCreated:
+                    break;
+                case MessageType.MigratedToSupergroup:
+                    break;
+                case MessageType.MigratedFromGroup:
+                    break;
+                case MessageType.Animation:
+                    break;
+                case MessageType.Poll:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Стартовая клавиатура
+        /// </summary>
+        /// <returns>Возвращает стартовую клавиатуру</returns>
+        private InlineKeyboardMarkup StartKeyboard()
+        {
+            var row = buttons.Where(x => x.ParentId == 0).OrderBy(x => x.Row).ThenBy(x => x.Column).GroupBy(x => x.Row).ToList();
+            var inlineKeyboard = new List<List<InlineKeyboardButton>>();
+            foreach (var item in row)
+            {
+                var listRowButtons = new List<InlineKeyboardButton>();
+                foreach (var i in item)
+                {
+                    listRowButtons.Add(InlineKeyboardButton.WithCallbackData($"{i.ButtonName}", i.Id.ToString()));
+                }
+                inlineKeyboard.Add(listRowButtons);
+            }
+
+            return new InlineKeyboardMarkup(inlineKeyboard);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Start()
+        {
+            //Проверяем доступность API (надо бы поправить, при недоступности валится в ошибку)
+            if (bot.TestApiAsync().Result)
+            {
+                Console.WriteLine("API доступен.");
+                bot.OnUpdate += UpdateListener;
+                bot.StartReceiving();
+                Console.WriteLine($"Token: {token}");
+            }
+            else
+            {
+                Console.WriteLine("API не доступен." +
+                    "\nПроверьте интернет соединение." +
+                    "\nИли возможно проблема с блокировкой.");
+            }
+        }
+
+        #endregion
+    }
+}
--- Homework_09/Program.cs
+++ Homework_09/Program.cs
@@ -16,8 +16,6 @@ namespace Homework_09
 {
     class Program
     {
-
-        static TelegramBotClient bot;
         static void Main(string[] args)
         {
             #region Задание
@@ -42,80 +40,13 @@ namespace Homework_09
 
             #endregion
 
-            #region Прокси
-            //Подходящий прокси найти не удалось
-            //https://hidemyna.me/ru/proxy-list/?maxtime=250#list
-
-            //var proxy = new WebProxy()
-            //{
-            //    Address = new Uri($"http://109.86.229.189:8080"),
-            //    UseDefaultCredentials = false
-            //};
-
-            //var httpClienHandler = new HttpClientHandler()
-            //{
-            //    Proxy = proxy
-            //};
-
-            //HttpClient hc = new HttpClient(httpClienHandler);
-            #endregion
-
             #region Старт Бота
 
-            string token = File.ReadAllText("token");
-
-            bot = new TelegramBotClient(token);
-
-            //при использовании прокси
-            //bot = new TelegramBotClient(token, hc);
-
-            if (bot.TestApiAsync().Result)
-            {
-                Console.WriteLine("Бот запущен.");
-            }
-
-            Console.WriteLine($"Token: {token}");
-
-            //bot.OnMessage += MessageListener; // подписываемся на событие получения сообщения
-            bot.OnUpdate += UpdateListener; // подписываемся на получение любых обновлений
-            
-            bot.StartReceiving();
+            MyBot myBot = new MyBot();
+            myBot.Start();
 
             Console.ReadKey();
             #endregion
-        }
-
-        private static void UpdateListener(object sender, UpdateEventArgs e)
-        {
-            switch (e.Update.Type)
-            {
-                case UpdateType.Unknown:
-                    break;
-                case UpdateType.Message:
-                    Helpers.AnswerOnMessage(bot, e);
-                    break;
-                case UpdateType.InlineQuery:
-                    break;
-                case UpdateType.ChosenInlineResult:
-                    break;
-                case UpdateType.CallbackQuery:
-                    Helpers.AnswerOnCallbackQerry(bot, e);
-                    break;
-                case UpdateType.EditedMessage:
-                    break;
-                case UpdateType.ChannelPost:
-                    break;
-                case UpdateType.EditedChannelPost:
-                    break;
-                case UpdateType.ShippingQuery:
-                    break;
-                case UpdateType.PreCheckoutQuery:
-                    break;
-                case UpdateType.Poll:
-                    break;
-                default:
-                    break;
-            }
-        }       
+        }    
     }
 }
