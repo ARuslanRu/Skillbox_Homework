b0f30e4588b03ee82978956368b47ed2d4789fd3
Руслан <agishevrus@gmail.com>
2020-12-04 20:03:17 +0300

    начало работы над добавлением базы данных. Частично сделан скрипт инициализации таблиц и стартовых данных.

---
+++ Homework_13/DataBase/InitScript.sql
@@ -0,0 +1,49 @@
+﻿drop table [dbo].[Departments]
+drop table [dbo].[Clients]
+drop table [dbo].[Accounts]
+drop table [dbo].[Deposites]
+
+create table [dbo].[Departments]
+(
+	[Id] int not null primary key identity, 
+	[ParentId] int not null,
+	[Name] nvarchar(50)
+)
+
+create table [dbo].[Clients]
+(
+	[Id] int not null primary key identity, 
+	[DepartmentId] int not null, 
+	[Name] nvarchar(50) not null
+)
+
+create table [dbo].[Accounts]
+(
+	[Id] int not null primary key identity,
+	[ClientId] int not null,
+	[Balance] decimal (10, 4) not null, 
+	[CreateDate] datetime not null
+)
+
+create table [dbo].[Deposites]
+(
+	[Id] int not null primary key identity,
+	[ClientId] int not null,
+	[Name] nvarchar(50),
+	[Balance] decimal(10,4),
+	[CreateDate] datetime not null,
+	[IsWithCapitalization] bit not null
+)
+
+
+set identity_insert [dbo].[Departments] on;
+
+insert into [dbo].[Departments] (Id, ParentId, [Name]) 
+values (1, 0, N'Департамент_01'),
+(2, 0, N'Департамент_02'),
+(3, 1, N'Департамент_03');
+
+set identity_insert [dbo].[Departments] off;
+
+--select * from [dbo].[Departments]
+
