a10f13223f7d4876ec004104c4f091626a559d98
Руслан <agishevrus@gmail.com>
2020-12-11 21:38:20 +0300

    Проект скопирован от проекта Homework_17. Переделан с использованием паттерна Dependency injection.

---
+++ Homework_18/Model/Department.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Homework_18.Model
+{
+    public class Department : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ Homework_18/ViewModel/DepartmentViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Homework_18.Helper;
+using Homework_18.Model;
+using System.Windows;
+
+namespace Homework_18.ViewModel
+{
+    class DepartmentViewModel : BaseViewModel
+    {
+        private Department department;
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DepartmentViewModel(Department department)
+        {
+            this.department = department;
+            Name = department.Name;
+        }
+
+        private RelayCommand confirmCommand;
+        public RelayCommand ConfirmCommand
+        {
+            get
+            {
+                return confirmCommand ??
+                    (confirmCommand = new RelayCommand(obj =>
+                    {
+                        department.Name = Name;
+                        Window window = obj as Window;
+                        window.DialogResult = true;
+                        window.Close();
+                    },
+                    obj => !string.IsNullOrEmpty(Name)));
+            }
+        }
+    }
+}
